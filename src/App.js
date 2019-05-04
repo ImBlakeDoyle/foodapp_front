bd570a9db01884b24a4d7767144a134f8311128b
Blake <blake.doyle@hotmail.com.au>
2019-05-03 17:07:27 -0700

    create component for home page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import { Route, Switch } from "react-router-dom";
 import './App.css';
-import { restElement } from '@babel/types';
+import HomePage from "./components/pages/HomePage";
+
+// function App() {
+//   const [allItems, addItem] = useState([]);
+//   const [item, setItem] = useState("");
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     addItem([...allItems, item]);
+//   }
+
+//   return (
+//     <form onSubmit = {handleSubmit}>
+//       <input 
+//       value={item}
+//       onChange={e => setItem(e.target.value)}
+//       />
+//       <strong>
+//         <p>Current item is: {item}</p>
+//       </strong>
+//       <ul>
+//         All items are: {allItems.map((i) => {
+//           return(
+//             <li key={i}>
+//               {i}
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </form>
+//   );
+// }
 
 function App() {
-  const [allItems, addItem] = useState([]);
-  const [item, setItem] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addItem([...allItems, item]);
-  }
-
   return (
-    <form onSubmit = {handleSubmit}>
-      <input 
-      value={item}
-      onChange={e => setItem(e.target.value)}
-      />
-      <strong>
-        <p>Current item is: {item}</p>
-      </strong>
-      <strong>
-        <ul>
-          All items are: {allItems.map((i) => {
-            return(
-              <li key={i}>
-                {i}
-              </li>
-            );
-          })}
-        </ul>
-      </strong>
-    </form>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+      </Switch>
+    </div>
   );
 }
 
